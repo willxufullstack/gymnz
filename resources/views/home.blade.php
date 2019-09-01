75f98fd3b16b94f1963839d38efea7fd92b2d210
Royn Wang <royn.wang.renyuan@gmail.com>
2019-09-01 22:17:15 +1200

    add js file version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
     var username = "{{ Auth::user() ? Auth::user()->name : "
     GUEST " }}";
 </script>
-<script src="{{ asset('js/manifest.js') }}" defer></script>
-<script src="{{ asset('js/vendor.js') }}" defer></script>
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/manifest.js') }}?v=1" defer></script>
+<script src="{{ asset('js/vendor.js') }}?v=1" defer></script>
+<script src="{{ asset('js/app.js') }}?v=1" defer></script>
 </html>
