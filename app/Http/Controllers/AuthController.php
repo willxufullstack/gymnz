d0525961228775a8d4a94ace98a02c4635c3a1bf
Royn Wang <royn.wang.renyuan@gmail.com>
2019-09-07 19:13:58 +1200

    fix token expire time

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -104,7 +104,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => $this->guard()->factory()->getTTL() * 60 * 24 * 365
+            'expires_in' => $this->guard()->factory()->getTTL() * 60
         ]);
     }
 
