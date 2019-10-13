0fa1e09ff7b22850467c9b6f91ef30e15b950d06
Renyuan Wang <rewang@pivotal.io>
2019-10-13 21:52:53 +1300

    [#169085957]import all order (#68)

--- app/Console/Commands/ImportCustomers.php
+++ app/Console/Commands/ImportCustomers.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Coach;
+use App\Gym;
+use App\Order;
 use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
@@ -41,6 +44,9 @@ class ImportCustomers extends Command
     {
         $phoneNumber = $this->argument('phoneNumber');
 
+        $coachUser = User::where('email', $phoneNumber)->first();
+        $defaultCoach = Coach::with('user')->where('user_id', $coachUser->id)->first();
+
         $url = 'http://o2-fit.com/api/' . $phoneNumber . '/customers/';
 
         $customersJson = json_decode(file_get_contents($url), true);
@@ -52,7 +58,6 @@ class ImportCustomers extends Command
             $user = User::where('email', $customer['name'])->first();
             if (empty($user)) {
                 $user = new User();
-                continue;
             }
             $user->password = Hash::make('00000000');
             $user->email = $customer['name'];
@@ -68,9 +73,59 @@ class ImportCustomers extends Command
                 echo ('ERROR: skipped customer ' . $user['displayname'] . ' ' . $user['name'] . "\n");
                 continue;
             }
-            echo ('imported customer ' . $user['displayname'] . ' ' . $user['name'] . "\n");
+            echo ('imported customer ' . $customer['displayname'] . ' : ' . $customer['name'] . "\n");
+            self::importOrders($customer['name'], $defaultCoach);
             $imported++;
         }
         echo "imported: $imported \n skipped: $skipped \n";
+    }
+
+    private static function importOrders($customerPhone, $defaultCoach)
+    {
+        $url = "http://o2-fit.com/api/$customerPhone/o/";
+        $ordersJson = json_decode(file_get_contents($url), true);
+        $orders = $ordersJson['results'];
+        foreach ($orders as $order) {
+            self::importOrder($order, $defaultCoach);
+        }
+    }
+
+    private static function importOrder($oriOrder, $defaultCoach)
+    {
+        echo "importing order {$oriOrder['customerdetail']['displayname']} +++ {$oriOrder['coachdetail']['displayname']} \n";
+        $coachUser = User::where('email', $oriOrder['coachdetail']['name'])->first();
+        $order = new Order();
+        // !!!!TODO
+        $order->created_by = $coachUser->id ?? $defaultCoach->user->id;
+        $order->price = $oriOrder['amount'];
+        $order->course_amount = $oriOrder['course_count'];
+        $order->duration = $oriOrder['duration'];
+
+        // format time
+        $formattedTime = str_replace('T', ' ', $oriOrder['created']);
+        $formattedTime = str_replace('Z', '', $formattedTime);
+        $order->created_at = $formattedTime;
+        // calcuate expiry
+        // !!!!TODO
+        // $order->expiry = Carbon::now()->addMonths($order->duration);
+        $order->expiry = $oriOrder['endtime'] !== 'N/A' ? $oriOrder['endtime'] : date("Y-m-d", time());
+        // 3. map user
+        // get customer
+        $customer = User::where('email', $oriOrder['custom'])->first();
+        $order->customer()->associate($customer);
+        // 4. map gym
+        $gym = Gym::where('name', $oriOrder['gym'])->first();
+        if(empty($gym)) {
+            $gym = Gym::find($defaultCoach->gym_id);
+        }
+        $order->gym()->associate($gym);
+        // 5. map coach
+        $coach = $defaultCoach;
+        if (!empty($coachUser)) {
+            $coach = Coach::where('user_id', $coachUser->id)->first();
+        }
+        $order->coach()->associate($coach);
+        // 6. return
+        $order->save();
     }
 }
