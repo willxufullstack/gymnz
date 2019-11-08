0a794b3d51799e824b3f8dc6eada8d73c82b52df
Renyuan Wang <rewang@pivotal.io>
2019-11-09 11:17:24 +1300

    [#169659686]use Qiniu to store js file (#78)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
     var username = "{{ Auth::user() ? Auth::user()->name : "
     GUEST " }}";
 </script>
-<script src="{{ asset('js/manifest.js') }}?v=1" defer></script>
-<script src="{{ asset('js/vendor.js') }}?v=1" defer></script>
-<script src="{{ asset('js/app.js') }}?v=1" defer></script>
+<script src="http://static.o2-fit.com/pro/js/manifest.js?v=1" defer></script>
+<script src="http://static.o2-fit.com/pro/js/vendor.js?v=1" defer></script>
+<script src="http://static.o2-fit.com/pro/js/app.js?v=1" defer></script>
 </html>
---
+++ script/uploadjs.sh
@@ -0,0 +1,4 @@
+qshell fput gymgo pro/js/vendor.js public/js/vendor.js --mimetype application/javascript --overwrite
+qshell fput gymgo pro/js/app.js public/js/app.js --mimetype application/javascript --overwrite
+qshell fput gymgo pro/js/manifest.js public/js/manifest.js --mimetype application/javascript --overwrite
+
