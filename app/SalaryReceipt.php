7b26c62c297a16cb395cebb897d922d76c265a55
Renyuan Wang <rewang@pivotal.io>
2019-11-10 22:23:02 +1300

    [#167457965]support calculate salary by percentage of course price (#79)

--- app/SalaryReceipt.php
+++ app/SalaryReceipt.php
@@ -50,11 +50,45 @@ class SalaryReceipt extends Model
             ->where('gym_id', $this->gym_id)
             ->where('date', '>=', date('Y-m-d', $start))
             ->where('date', '<=', date('Y-m-d', $end))
-            ->where('status', 2);
+            ->where('status', 2)
+            ->get();
+
+        $moneyByCoursePercentage = 0;
+        // get percentage by price
+        if ($this->course_percentage) {
+            foreach ($query as $s) {
+                $money = 120;
+                $p = $s->getPrice();
+                if ($p) {
+                    $money = $p * $this->course_percentage / 100;
+                }
+                $moneyByCoursePercentage += $money;
+            }
+        }
+
+        $moneyByOrderPercentage = 0;
+        if ($this->sale_percentage) {
+            $orders = Order::where('coach_id', $this->coach_id)
+                ->where('gym_id', $this->gym_id)
+                ->where('created_at', '>=', date('Y-m-d H:i:s', $start))
+                ->where('created_at', '<=', date('Y-m-d H:i:s', $end))
+                ->get();
+            foreach ($orders as $order) {
+                $moneyByOrderPercentage += $order->price;
+            }
+            $moneyByOrderPercentage = $moneyByOrderPercentage * $this->sale_percentage / 100;
+        }
+
 
         // update total
-        $count = $query->count();
+        // $count = $query->count();
+        $count = count($query);
         $this->course_count = $count;
-        $this->total = $this->base + $count * $this->course_fixed + $this->adjustment - $this->tax;
+        $this->total = $this->base
+            + $count * $this->course_fixed
+            + $this->adjustment
+            + $moneyByCoursePercentage
+            + $moneyByOrderPercentage
+            - $this->tax;
     }
 }
