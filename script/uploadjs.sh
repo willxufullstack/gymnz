52a9da3428d0450ce2cd3fa521af1ce78db3a9bf
Royn Wang <royn.wang.renyuan@gmail.com>
2020-01-17 21:01:40 +1300

    update gitignore

--- script/uploadjs.sh
+++ script/uploadjs.sh
@@ -1,4 +1,10 @@
-qshell fput gymgo pro/js/vendor.js public/js/vendor.js --mimetype application/javascript --overwrite
-qshell fput gymgo pro/js/app.js public/js/app.js --mimetype application/javascript --overwrite
-qshell fput gymgo pro/js/manifest.js public/js/manifest.js --mimetype application/javascript --overwrite
-
+#qshell fput gymgo pro/js/vendor.js public/js/vendor.js --mimetype application/javascript --overwrite
+#qshell fput gymgo pro/js/app.js public/js/app.js --mimetype application/javascript --overwrite
+#qshell fput gymgo pro/js/manifest.js public/js/manifest.js --mimetype application/javascript --overwrite
+for i in {1..20}
+do
+  if [ -e ./public/$i.js ]
+  then
+    qshell fput gymgo $i.js public/$i.js --mimetype application/javascript --overwrite
+  fi
+done
