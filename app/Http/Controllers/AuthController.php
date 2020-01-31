8df5994c31d70ed2899e0385fe4bdfc4eebd86d5
Renyuan Wang <rewang@pivotal.io>
2020-01-31 22:34:13 +1300

    [NZHelper] set appid to allow client to fetch openid (#129)
    still need to replace the secrect

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'openid', 'bind', 'token', 'chart']]);
+        $this->middleware('auth:api', ['except' => ['login', 'openid', 'bind', 'token', 'chart', 'NZHelperOpenId']]);
     }
 
     /**
@@ -115,6 +115,24 @@ class AuthController extends Controller
     public function guard()
     {
         return Auth::guard('api');
+    }
+    public function NZHelperOpenId(Request $request)
+    {
+        $code = $request->input('code');
+        $appId = 'wx6cda57a3aa04e5aa';
+        $secret = config('services.wx.secret'); // TODO replace
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' . $appId . '&secret=' . $secret . '&js_code=' . $code . '&grant_type=authorization_code';
+        $json = json_decode(file_get_contents($url), true);
+        $openid = $json['openid'];
+        $ret = [
+            'openid' => $openid
+        ];
+        $user = User::where('openid', $openid)->first();
+        if(!empty($user)){
+            $ret['token'] = $this->guard()->tokenById($user->id);
+        }
+
+        return response()->json($ret);
     }
 
     public function openid(Request $request)
