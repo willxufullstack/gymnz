760730eda8f20de97ea93e36834d332a699229e7
Royn Wang <royn.wang.renyuan@gmail.com>
2020-05-13 22:34:11 +1200

    hide api_token

--- app/User.php
+++ app/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'updated_at', 'email_verified_at',
+        'password', 'remember_token', 'updated_at', 'email_verified_at', 'api_token'
     ];
 
     /**
